a7420fd366da3d2901a7f303f892337127494226
lgs85 <lewisspurgin@gmail.com>
2018-05-22 17:53:01 +0100

    Edits

--- TriboliumInvasion_PLOTS.R
+++ TriboliumInvasion_PLOTS.R
@@ -3,6 +3,9 @@ library(Rmisc)
 
 s1 <- read.csv("Dispersal_selection.csv")
 
+head(s1)
+
+
 f1 <-
   ggplot(s1,
          aes(
@@ -18,8 +21,11 @@ f1 <-
   theme_classic() +
   theme(legend.position = "none")+
   annotate(geom = "text",x = c(6,9),y = c(0.75,0.15),label = c("High dispersal lines","Low dispersal lines"))+
+  scale_colour_manual(values = c("orange","green"))+
+  scale_fill_manual(values = c("orange","green"))+
   ylab ("Dispersal rate")
 
+f1
 
 h1 <- read.csv("Dispersal_H2.csv")
 h1$Cross <- paste(h1$Female, h1$Male, sep = " x ")
@@ -33,3 +39,6 @@ f2 <-
   ylab ("Dispersal rate")
 
 multiplot(f1, f2)
+
+ggplot(s1,aes(x = Dispersal))+
+  geom_density(lty = 0,fill = "grey")
